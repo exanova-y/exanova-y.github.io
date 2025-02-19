6997520bb767815088e5581dab0140dfce1db9e6
Yoyo Yuan <yoyoyuan1729@gmail.com>
2025-02-18 16:45:37 -0800

    meow

--- _plugins/media_url_filter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module MediaUrlFilter
-    def media_url(input)
-      "https://media.githubusercontent.com/media/exanova-y/exanova-y.github.io/main#{input}"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::MediaUrlFilter)
